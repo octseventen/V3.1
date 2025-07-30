9ca0131b95829c43c44374ea3bb577951473860b
LordFreedom <148976788+octseventen@users.noreply.github.com>
2025-07-30 14:38:57 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -84,7 +84,7 @@ echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━�
 echo -e " \e[1;97;101m               UPDATE SCRIPT            \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "  \033[1;91m update script service\033[1;37m"
+echo -e "  \033[1;91m Update In Progres \033[1;37m"
 fun_bar 'res1'
 fun_bar 'res2'
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
